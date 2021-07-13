b08b806a6399dc44e94284564b630d42c25fa8e9
Eden Shtein <edens@academy-skills.com>
2021-07-13 14:29:44 +0300

    init

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+import {Platform} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
+
+import MainFeedScreen from './src/screens/MainFeedScreen';
+import {useEffect} from 'react';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MainFeedScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/screens/MainFeedScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const MainFeedScreen = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+//const styles = StyleSheet.create({});
+
+export default MainFeedScreen;
