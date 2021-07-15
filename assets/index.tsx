4b1a1c7e3d7265e5a5f99c8105f9ce259318332e
edenshteinskills <86909176+edenshteinskills@users.noreply.github.com>
2021-07-15 17:09:30 +0300

    Create main article card,fake data for articles and interval mechanism (#4)

---
+++ assets/index.tsx
@@ -0,0 +1,5 @@
+import article1 from './main_articles/image1.png';
+import article2 from './main_articles/image2.png';
+import article3 from './main_articles/image3.png';
+
+export {article1, article2, article3};
--- src/screens/MainFeedScreen.tsx
+++ src/screens/MainFeedScreen.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View} from 'react-native';
+import MainArticle from '../components/MainArticle';
+import articles from '../../fake_db/fake_main';
+import Article from '../types';
 
 const MainFeedScreen = () => {
+  const [article, setArticle] = useState<Article>({});
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setArticle(articles[Math.floor(Math.random() * articles.length)]);
+    }, 30000); //change article every 3 minutes
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <View>
-      <Text>Home Screen</Text>
+      <MainArticle
+        title={article.title}
+        url={article.url}
+        author={article.author}
+      />
     </View>
   );
 };
