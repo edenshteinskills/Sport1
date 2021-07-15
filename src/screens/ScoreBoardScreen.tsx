2db4dd91f53649462d190fcc2ad63e690ecd93c5
edenshteinskills <86909176+edenshteinskills@users.noreply.github.com>
2021-07-15 11:38:28 +0300

    Added bottom tab bar (#3)

---
+++ src/screens/ScoreBoardScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const ScoreBoardScreen = () => {
+  return (
+    <View>
+      <Text>ScoreBoard Screen</Text>
+    </View>
+  );
+};
+
+//const styles = StyleSheet.create({});
+
+export default ScoreBoardScreen;
